ce42154f8b5b5bd23894fd9a6c977a05db64e667
NakuraTakatsugu <arukan.b.b@gmail.com>
2023-04-19 02:27:04 +0900

    fix 文言の修正

--- src/components/ResultScreen.tsx
+++ src/components/ResultScreen.tsx
@@ -15,8 +15,15 @@ export const ResultScreen: React.FC<ResultScreenProps> = ({ score, totalQuestion
       <p>
         あなたは{totalQuestions}問中、{score}問正解しました.
       </p>
-      {isPerfectScore && <p>おめでとう！全問正解です！</p>}
-      <button onClick={onRestart}>全問正解を目指す</button>
+      {isPerfectScore &&
+        <>
+          <p>全問正解です！おめでとうございます！</p>
+          <br></br>
+          <a href="." className="href">話を聞きにいく</a>
+        </>
+      }
+      {!isPerfectScore && <button onClick={onRestart}>全問正解を目指す</button>}
+      <button onClick={onRestart}>戻る</button>
     </div>
   );
 };
